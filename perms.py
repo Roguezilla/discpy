e8e9a8d4b1a469d732b2b3232d392a3f7a2f040e
Olek <roguezilla0@gmail.com>
2021-12-11 16:00:24 +0000

    ratelimits for real

--- perms.py
+++ perms.py
@@ -5,4 +5,4 @@ async def basic_perms_check(self: DiscPy, msg: Message):
     return await self.has_permissions(msg, self.Permissions.MANAGE_MESSAGES)
 
 async def basic_perms_check2(self: DiscPy, msg: Message):
-    return await self.has_permissions(msg, self.Permissions.MANAGE_MESSAGES) or self.is_owner(msg.author.id)
+    return await self.has_permissions(msg, self.Permissions.MANAGE_MESSAGES) or await self.is_owner(msg.author.id)
